218d1480463144b77c508c0d6f7c77a4e2fd224a
LucoMoro <morelli.2000luca@gmail.com>
2024-11-11 20:14:56 +0100

    feat: add code to analyze retweets

--- sad_script.R
+++
@@ -1,9 +0,0 @@
-library(readr)
-
-#setwd(TO SET)
-
-data <- read.csv("Sentiment_it_tweet_2023.csv", sep = ";", header = TRUE)
-
-frequency <- table(data$sentiment)
-print(frequency)
-plot(frequency)
